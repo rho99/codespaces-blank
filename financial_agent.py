2c811809080d65ad7ec6ac011689e5929be2dc2a
Bokkisam Rohit <31767498+rho99@users.noreply.github.com>
2024-12-28 12:18:30 +0000

    Initial package Changes

---
+++ financial_agent.py
@@ -0,0 +1,8 @@
+from phi.agent import Agent
+from phi.model.groq import Groq
+from phi.tools.yfinance import YFinanceTools
+from phi.tools.duckduckgo import DuckDuckGo
+
+web_search_agent = Agent(
+    
+)
